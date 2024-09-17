412784e25fa24e1b521043aca8ea2408ab66ab07
Hasan Küçükselek <hasankucukselek7@gmail.com>
2024-09-17 13:38:36 +0300

    Proje Güncellendi

--- src/test/java/stepDefinitions/merchanPageStepDef/MerchantLoginStepDef.java
+++ src/test/java/stepDefinitions/merchanPageStepDef/MerchantLoginStepDef.java
@@ -5,7 +5,7 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import org.junit.Assert;
 import pages.homePages.HomePage;
-import pages.MerchantPages.MerchantLoginPage;
+import pages.merchantPages.MerchantLoginPage;
 import utilities.ConfigLoader;
 import utilities.ExcelDataReader;
 import utilities.ReusableMethods;
