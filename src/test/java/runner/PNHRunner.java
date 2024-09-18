9799bd1470ea47ee19db57291f8993d984e78375
ghajarpouneh@gmail.com <164321458+eghbalghajar@users.noreply.github.com>
2024-09-18 12:51:36 +0330

    PNH_1_

---
+++ src/test/java/runner/PNHRunner.java
@@ -0,0 +1,17 @@
+package runner;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+
+		features = "src/test/resources/features", glue = { "stepDefinitions" },
+		plugin = { "pretty", "html:target/reports/cucumber-reports.html", "json:target/reports/cucumber.json",
+				"junit:target/reports/Cucumber.xml", },
+		tags = "", dryRun = false, publish = true)
+
+public class PNHRunner {
+
+}
