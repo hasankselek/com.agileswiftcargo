d129ea883ede70c4ed7cae2fa5f538cc32be769d
Gül Akyol <msakyol@outlook.com>
2024-09-19 10:24:02 +0200

    US_34 Gulcihan/19/09/24/10UHR

---
+++ src/test/java/stepDefinitions/adminPageStepDef/AdminBrancheManagePaymentsStepDef.java
@@ -0,0 +1,46 @@
+package stepDefinitions.adminPageStepDef;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+
+public class AdminBrancheManagePaymentsStepDef {
+    @And("Click on Branch Manage")
+    public void clickOnBranchManage() {
+        
+    }
+
+    @And("Click on the Payments Icon")
+    public void clickOnThePaymentsIcon() {
+        
+    }
+    @And("Test the visibility of the Branch Payment list")
+    public void testTheVisibilityOfTheBranchPaymentList() {
+        
+    }
+    @And("Test the visibility of the + sign in the upper right corner of the list")
+    public void testTheVisibilityOfTheSignInTheUpperRightCornerOfTheList() {
+        
+    }
+    @And("Click on the (+) sign")
+    public void clickOnTheSign() {
+        
+    }
+
+    @And("test the visibility of the Create Payment page")
+    public void testTheVisibilityOfTheCreatePaymentPage() {
+    }
+
+    @And("Click on Payments")
+    public void clickOnPayments() {
+        
+    }
+
+    @And("Test the visibility of the Actions Icon")
+    public void testTheVisibilityOfTheActionsIcon() {
+        
+    }
+
+    @And("test the visibility of links in the menu")
+    public void testTheVisibilityOfLinksInTheMenu() {
+    }
+}
